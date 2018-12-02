1c5074a289cd3a3c65829347e465a6d5b6c48b88
Kugler Krisztián <krisztian.kugler@gmail.com>
2018-12-02 22:55:46 +0100

    Updates

---
+++ precache-manifest.e67c63aa105752d3f45590d6b553d53a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/quince-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c85c8174b30e166161e2",
+    "url": "/quince-react/static/js/runtime~main.c85c8174.js"
+  },
+  {
+    "revision": "e2b8c9ce94256e0be465",
+    "url": "/quince-react/static/js/main.e2b8c9ce.chunk.js"
+  },
+  {
+    "revision": "259a708ff9367f32473c",
+    "url": "/quince-react/static/js/1.259a708f.chunk.js"
+  },
+  {
+    "revision": "e2b8c9ce94256e0be465",
+    "url": "/quince-react/static/css/main.a891fe38.chunk.css"
+  },
+  {
+    "revision": "7f16d8cb4cf5dacdafb69a270dffc931",
+    "url": "/quince-react/index.html"
+  }
+];
